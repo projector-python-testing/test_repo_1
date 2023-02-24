2055884425f9b51ba9c70475576c7937f3d407e1
chepuralex1991 <60858989+chepuralex1991@users.noreply.github.com>
2023-02-24 17:56:09 +0000

    Add files via upload
    Request to API

---
+++ request.py
@@ -0,0 +1,9 @@
+﻿import requests
+
+access_token = "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"
+repo_owner = "projector-python-testing"
+repo_name = "test_repo_1"
+
+url = f'https://api.github.com/repos/{repo_owner}/{repo_name}'
+response = requests.get(url)
+print(response)
